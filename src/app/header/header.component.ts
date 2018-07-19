0cac15246c179eda81a750807a6fbe3fec8d0a5c
usmankhanbrainplow <usman.khanbrainplow.com>
2018-07-19 15:19:04 +0500

    sad

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,9 @@ import { LoginService } from '../pages/login/login.service';
 import { ViewChild } from '@angular/core';
 import { RecaptchaComponent } from 'recaptcha-blackgeeks';
 import { HeaderService } from './header.service';
+import { DataService } from '../data.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 declare var $: any;
 
 @Component({
@@ -29,7 +32,7 @@ export class HeaderComponent implements OnInit {
   zipcode;
   record: any = []
   zipCode;
-  constructor(private router: Router, private _serv: HeaderService, private fb: FormBuilder, private http: HttpClient, private route: ActivatedRoute, private sg: SimpleGlobal) { }
+  constructor(private router: Router, private _serv: HeaderService,  private data: DataService, private fb: FormBuilder, private http: HttpClient, private route: ActivatedRoute, private sg: SimpleGlobal) { }
   checked_login() {
     if (localStorage.getItem('custum')) {
       let local = localStorage.getItem('custum');
@@ -87,7 +90,7 @@ export class HeaderComponent implements OnInit {
   }
   submit(event,query){
     if (event.key == "Enter") {
-     
+     this.data.zipcodeInfo(query)
     let sth = 'products/'+query;
     this.router.navigate([sth]);
     localStorage.setItem('zip',query);
@@ -96,6 +99,8 @@ export class HeaderComponent implements OnInit {
     console.log(query)
     this._serv.searchrecord(query).subscribe(data => {
       this.record = data
+      // this.sg['zip'] = Res.json()['Results'];
+      // this.data.changezip(this.sg['zip']);
       console.log(this.record)
     }, error => {
 
@@ -105,14 +110,18 @@ export class HeaderComponent implements OnInit {
   
   singlerfp(zipcode){
     console.log(localStorage.setItem('zip',zipcode))
+    // this.sg['zip'] = Res.json()['Results'];
+      this.data.zipcodeInfo(zipcode)
     let sth = 'products/'+zipcode;
     this.router.navigate([sth]);
+    // this.data.changezip(this.sg['zip']);
     localStorage.setItem('zip',zipcode);
     console.log(zipcode)
   }
   onKeydown(event) {
   
     if (event.key === "Enter") {
+
       this.singlerfp(this.zipcode)
       // this.router.navigate(['/products/' + this.zipCode]);
       // localStorage.setItem('zip', this.zipCode);
