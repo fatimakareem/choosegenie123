6ca5ebc56c3a946d1f9e17bac158a121875cc231
fatimakarim <you@example.com>
2018-07-23 19:04:12 +0500

    ssssss

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -76,12 +76,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
      name;
     obj:any=[];
     editdata: any = [];
-<<<<<<< HEAD
     search(name,page: number) {
-=======
-    date = new FormControl(new Date());
-    search(name) {
->>>>>>> c85048e4e401cf7bcd0f5be5e9ab349b073d2a8d
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
         //   this.http.get(Config.api + 'data_against_zipcode/' + this.zip_code + '', { headers: headers }),
