625105b47d4d14c7a0524e13f3e1bf7408316c81
usmankhanbrainplow <usman.khanbrainplow.com>
2018-07-28 18:15:23 +0500

    knmn

--- src/app/Config.ts
+++ src/app/Config.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Config {//https://apis.choicegenie.com/choice/
     //public static api: String = 'https://apis.choicegenie.com/choice/';
- //public static api: String = 'https://apis.choicegenie.com/';
+//  public static api: String = 'https://apis.choicegenie.com/';
  public static api: String = 'http://192.168.30.119:9000/';
    public static Imageurl: string = 'https://storage.choicegenie.com/media/'; 
     public  static  Imageurlget = 'https://storage.choicegenie.com/images/';  
