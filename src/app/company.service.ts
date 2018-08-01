75bc0b25f329a65b3a6fceb9e1a61aec4b68a0f9
fatimakarim <you@example.com>
2018-08-01 10:37:55 +0500

    dddddddd

--- src/app/company.service.ts
+++ src/app/company.service.ts
@@ -13,26 +13,13 @@ id;
 username;
   searchProduct(title,page) {
     console.log(this.username)
-   // let headers = new Headers({'Authorization': 'JWT ' + username.token});
    let headers = new Headers();
-  // headers.append('Content-Type', 'application/json');
-  // headers.append('Access-Control-Allow-Headers', 'Content-Type');
-  // headers.append('Access-Control-Allow-Methods', 'GET');
     return this.https.get(Config.api + 'mydata/'+ this.username +'/'+'?page='+page, {headers: headers} ) .map((response: Response)  => response);
- // return this.https.get('http://192.168.30.52:9000/choice/mydata/'+ username + '/').map((response: Response) => response.json());
-//mydata
-
     }
     deregulatedsearch(title,page) {
       console.log(this.username)
-     // let headers = new Headers({'Authorization': 'JWT ' + username.token});
      let headers = new Headers();
-    // headers.append('Content-Type', 'application/json');
-    // headers.append('Access-Control-Allow-Headers', 'Content-Type');
-    // headers.append('Access-Control-Allow-Methods', 'GET');
       return this.https.get(Config.api + 'titlescompanies/'+ this.username +'?page='+page, {headers: headers} ) .map((response: Response)  => response);
-   // return this.https.get('http://192.168.30.52:9000/choice/mydata/'+ username + '/').map((response: Response) => response.json());
-  //mydata
   
       }
    
