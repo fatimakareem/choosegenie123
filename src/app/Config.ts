b47d47fdcca81f2cd04d515a813184c65b6e59b2
fatimakarim <you@example.com>
2018-08-07 19:07:24 +0500

    aaaa

--- src/app/Config.ts
+++ src/app/Config.ts
@@ -3,13 +3,8 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Config {//https://apis.choicegenie.com/choice/
     //public static api: String = 'https://apis.choicegenie.com/choice/';
-<<<<<<< HEAD
 // public static api: String = 'https://apis.choicegenie.com/';
   public static api: String = 'http://192.168.30.237:9000/';
-=======
- public static api: String = 'https://apis.choicegenie.com/';
-//  public static api: String = 'http://192.168.30.237:9000/';
->>>>>>> 84baa85e73c53bfeed183fdd59460695b50d32b6
    public static Imageurl: string = 'https://storage.choicegenie.com/media/'; 
     public  static  Imageurlget = 'https://storage.choicegenie.com/images/';  
    public  static  Imageurlupload = 'https://storage.choicegenie.com/upload_image.php';
