5372a7bea3d152f738f7938e41acbb16d9d9c2dd
usmankhanbrainplow <usman.khanbrainplow.com>
2018-08-15 10:14:45 +0500

    cv vc

--- src/app/Config.ts
+++ src/app/Config.ts
@@ -4,11 +4,7 @@ import {Injectable} from '@angular/core';
 export class Config {//https://apis.choicegenie.com/choice/
     //public static api: String = 'https://apis.choicegenie.com/choice/';
 public static api: String = 'https://apis.choicegenie.com/';
-<<<<<<< HEAD
-//    public static api: String = 'http://192.168.30.237:9000/';
-=======
   // public static api: String = 'http://192.168.30.237:9000/';
->>>>>>> 6132cc669f0f11a7b6dec7f5c6ff1900f47e2cbc
    public static Imageurl: string = 'https://storage.choicegenie.com/media/'; 
     public  static  Imageurlget = 'https://storage.choicegenie.com/images/';  
    public  static  Imageurlupload = 'https://storage.choicegenie.com/upload_image.php';
