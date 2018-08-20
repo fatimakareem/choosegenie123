f9436ee72adaf94d86f80696783a13579ae576b7
usmankhanbrainplow <usman.khanbrainplow.com>
2018-08-20 10:33:48 +0500

    asdadad

--- src/app/Config.ts
+++ src/app/Config.ts
@@ -3,16 +3,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Config {//https://apis.choicegenie.com/choice/
     //public static api: String = 'https://apis.choicegenie.com/choice/';
-<<<<<<< HEAD
-// public static api: String = 'https://apis.choicegenie.com/';
-  public static api: String = 'http://192.168.30.238:9000/';
-=======
 public static api: String = 'https://apis.choicegenie.com/';
 // public static api: String = 'http://192.168.30.238:7000/';
 
 //   public static api: String = 'http://192.168.30.238:9000/';
 
->>>>>>> 3a46968822cb7cb15297b4ceb2d9b39d7de22c57
    public static Imageurl: string = 'https://storage.choicegenie.com/media/'; 
     public  static  Imageurlget = 'https://storage.choicegenie.com/images/';  
    public  static  Imageurlupload = 'https://storage.choicegenie.com/upload_image.php';
