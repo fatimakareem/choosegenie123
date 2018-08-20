402f6889aaffe1c7e435ffb1c04fde2563323bd1
fatimakarim <you@example.com>
2018-08-20 18:59:53 +0500

    ww

--- src/app/Deregulated/product/product.component.ts
+++ src/app/Deregulated/product/product.component.ts
@@ -190,10 +190,7 @@ this. featuredplan();
         this.router.navigate(['/Review/' + id]);
         localStorage.setItem('company', title);
     }
-    plan(plan) {
-        this.plan_information = plan;
-        console.log('id : ' + this.id, this.title);
-    }
+   
     btnratingClick(id, title, profileurl, profile_logo, servicearea) {
         this.id = id;
         this.comtitle = title;
