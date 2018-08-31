04caaf1bf19261bfce6d801e5e06a0f352efd67e
fatimakarim <you@example.com>
2018-08-31 14:28:10 +0500

    aaa

--- src/app/Blogchoicegenie/viewblog/blog1.module.ts
+++ src/app/Blogchoicegenie/viewblog/blog1.module.ts
@@ -3,19 +3,21 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 // import { MdModule } from '../md/md.module';
 import { MaterialModule } from '../../app.module';
-
+// import { GridModule } from '@progress/kendo-angular-grid';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatDatepickerModule} from "@angular/material";
 import { HttpClientModule } from '@angular/common/http';
 import { Blog1Routes } from './blog1.routing';
 import { Blog1Component } from './blog1.component';
 import { LoaderModule } from '../../loader/loader.module';
+import '@progress/kendo-ui';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild(Blog1Routes),
         // MdModule,
+        // GridModule,
         MaterialModule,
         LoaderModule,
         FormsModule,
