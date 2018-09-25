f425daed1cb468389c5700877a82556f72f09da4
unknown <you@example.com>
2018-09-25 17:41:48 +0500

    hdjejh

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -9,6 +9,7 @@ public username;
   months:any[];
   product;
   items;
+  com;
   constructor(private http: Http) {this.username = localStorage.getItem('username');
                this.product = localStorage.getItem('PRO');
 }
@@ -34,124 +35,44 @@ public username;
    return this.http.get(Config.api+'deregulated_inactive_product/' + this.username +'?page='+page +'/').map((response: Response) => response);
 
   }
-  filter(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,price,sort,item,min,max) {
- console.log(price)
-    if(price == undefined){
-    
-      console.log(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,price,sort,item,min,max)
-      const headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      return this.http.post(Config.api+'multifilter/'+id +'?page='+page, JSON.stringify({
-        "plan_type1": fixed,
-        "plan_type2": market,
-        "plan_type3": vari,
-        "plan_information1": months1,
-        "plan_information2": months2,
-        "plan_information3": months3,
-        "plan_information4": months4,
-        "plan_information5": months5,
-        "plan_information6": months6,
-        "prepaid": prepaid,
-        "noprepaid": notprepaid,
-        "planmin": planmin,
-        "time":time,
-        "notime":nottime,
-        "renewablerate":renewable,
-        "company":name,
-        "itemsperpage":"10",
-        "dsc":sort
+  filter(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,sort,item,min,max,logo1,logo2,logo3,logo4,logo5) {
+if(name){
+    this.com=name.trim();}
+    console.log(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,sort,item,min,max)
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(Config.api+'multifilter/'+id +'?page='+page, JSON.stringify({
+      "plan_type1": fixed,
+      "plan_type2": market,
+      "plan_type3": vari,
+      "plan_information1": months1,
+      "plan_information2": months2,
+      "plan_information3": months3,
+      "plan_information4": months4,
+      "plan_information5": months5,
+      "plan_information6": months6,
+      "prepaid": prepaid,
+      "noprepaid": notprepaid,
+      "planmin": planmin,
+      "time":time,
+      "notime":nottime,
+      "renewablerate":renewable,
+      "company":this.com,
+      "itemsperpage":"10",
+      "min_price": min,
+      "max_price": max,
+      "dsc":sort,
+      "logo1":logo1,
+      "logo2":logo2,
+      "logo3":logo3,
+      "logo4":logo4,
+      "logo5":logo5
+      
 
-       }), 
-      {headers: headers}).map((response: Response) => response.json());
-      }
-    else if(price==500){
-    console.log(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,price,sort,item,min,max)
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post(Config.api+'multifilter/'+id +'?page='+page, JSON.stringify({
-      "plan_type1": fixed,
-      "plan_type2": market,
-      "plan_type3": vari,
-      "plan_information1": months1,
-      "plan_information2": months2,
-      "plan_information3": months3,
-      "plan_information4": months4,
-      "plan_information5": months5,
-      "plan_information6": months6,
-      "prepaid": prepaid,
-      "noprepaid": notprepaid,
-      "planmin": planmin,
-      "time":time,
-      "notime":nottime,
-      "renewablerate":renewable,
-      "company":name,
-      "itemsperpage":"10",
-      "price_500_kwh_min_price": min,
-      "price_500_kwh_max_price": max,
-      "dsc":sort
      }), 
     {headers: headers}).map((response: Response) => response.json());
     }
   
- else if(price==1000){
-    console.log(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,price,sort,item,min,max)
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post(Config.api+'multifilter/'+id +'?page='+page, JSON.stringify({
-      "plan_type1": fixed,
-      "plan_type2": market,
-      "plan_type3": vari,
-      "plan_information1": months1,
-      "plan_information2": months2,
-      "plan_information3": months3,
-      "plan_information4": months4,
-      "plan_information5": months5,
-      "plan_information6": months6,
-      "prepaid": prepaid,
-      "noprepaid": notprepaid,
-      "planmin": planmin,
-      "time":time,
-      "notime":nottime,
-      "renewablerate":renewable,
-      "company":name,
-      "itemsperpage":"10",
-      "price_1000_kwh_min_price": min,
-      "price_1000_kwh_max_price": max,
-      "dsc":sort
-     }), 
-    {headers: headers}).map((response: Response) => response.json());
-    }
-  
- else if(price==2000){
-    console.log(page,id,months1,months2,months3,months4,months5,months6,months7,fixed,vari,market,notprepaid,prepaid,planmin,time,nottime,renewable,name,price,sort,item,min,max)
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post(Config.api+'multifilter/'+id +'?page='+page, JSON.stringify({
-      "plan_type1": fixed,
-      "plan_type2": market,
-      "plan_type3": vari,
-      "plan_information1": months1,
-      "plan_information2": months2,
-      "plan_information3": months3,
-      "plan_information4": months4,
-      "plan_information5": months5,
-      "plan_information6": months6,
-      "prepaid": prepaid,
-      "noprepaid": notprepaid,
-      "planmin": planmin,
-      "time":time,
-      "notime":nottime,
-      "renewablerate":renewable,
-      "company":name,
-      "itemsperpage":"10",
-        "price_2000_kwh_min_price": min,
-        "price_2000_kwh_max_price": max,
-      "dsc":sort
-     }), 
-    {headers: headers}).map((response: Response) => response.json());
-    }
- 
-  }
   deregulatedfilter(page,zip_code,months1,months2,months3,months5,months6,items,dsc,com,min,max) {
     // console.log(price)
       if(items==undefined){
