1a420cbe0a9d987b4dc0c31a47557ef54a66a1f9
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2018-10-18 19:46:41 +0500

    sadsad

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -84,6 +84,13 @@ googleTranslateElementInit() {
   moving1() {
     this.router.navigate(['/supermaindashboard']);
   }
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+ closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
   ngOnInit() {
     
     const mainSearch = $('.main-search');
@@ -108,6 +115,33 @@ googleTranslateElementInit() {
 this.admin=localStorage.getItem('currentadmin')
     this.username = localStorage.getItem('username')
     console.log(this.username);
+    // <script>
+      $("#showmenu").click(function(e){
+        e.preventDefault();
+        $("#menu").toggleClass("show");
+      });
+      $("#menu a").click(function(event){
+        event.preventDefault();
+        if($(this).next('ul').length){
+          $(this).next().toggle('fast');
+          $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
+        }
+      });
+    // </script>
+    // <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-36251023-1']);
+      _gaq.push(['_setDomainName', 'jqueryscript.net']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    // </script>
 
   }
   logout() {
